ee184326ed7c80994a88dd71cb01ca926d18a935
preciousimo <preciousimoniakemu@gmail.com>
2023-06-24 16:06:48 +0100

    Setup corsheaders

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RoomView, CreateRoomView
 
 urlpatterns = [
-    path('room', RoomView.as_view()),
-    path('create-room', CreateRoomView.as_view())
+    path('room/', RoomView.as_view()),
+    path('create-room/', CreateRoomView.as_view())
 ]
